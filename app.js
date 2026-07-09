fdcbf5022fedd0313d19bab93f482ed5fb04e32e
Suhani Nimbalkar <nimbalkarsuhani02@gmail.com>
2026-07-09 13:03:54 +0530

    Implemented server-side validation and improved UI

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+const { log } = require("console");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,11 +36,22 @@ app.get("/",(req,res)=>{
     res.send("root");
 });
 
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
 //Index Route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({});
     res.render("listings/index",{allListings});
-});
+})
+);
 
 //New Route
 app.get("/listings/new",(req,res)=>{
@@ -44,11 +59,12 @@ app.get("/listings/new",(req,res)=>{
 });
 
 //Show Route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show",{listing});
-});
+})
+);
 
 //Create Route
 /*app.post("/listings",async(req,res)=>{
@@ -56,36 +72,40 @@ app.get("/listings/:id",async(req,res)=>{
     await newListing.save();
     res.redirect("/listings");
 });*/
-app.post("/listings", async (req, res) => {
-    let url = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200";
-    let filename = "listingimage";
-    const newListing = new Listing(req.body.listing);
-    newListing.image = { url, filename };
-    await newListing.save();
-    res.redirect("/listings");
-});
+app.post("/listings",validateListing, wrapAsync(async(req, res) => {
+        let url = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200";
+        let filename = "listingimage";
+        const newListing = new Listing(req.body.listing);
+        newListing.image = { url, filename };
+        await newListing.save();
+        res.redirect("/listings");
+})
+);
 
 //Edit Route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit",{listing});
-});
+})
+);
 
 //Update Route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+})
+);
 
 //Delete Route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+})
+);
 
 /*app.get("/testListing",async(req,res)=>{
     let sampleListing=new Listing({
@@ -99,6 +119,16 @@ app.delete("/listings/:id",async(req,res)=>{
     console.log("sample was saved");
     res.send("successfull testing");
 });*/
+
+app.all("/{*any}",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
+});
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something Went Wrong"}=err;
+    res.status(statusCode).render("error.ejs",{message});
+    //res.status(statusCode).send(message);
+});
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
